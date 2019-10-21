03edb8dc088112e817fbc974c28528ec0f216429
Gabriel Ciulei <ciulei.gabriel@gmail.com>
2019-10-21 20:01:59 +0200

    create comment

--- website/src/models/comment.php
+++ website/src/models/comment.php
@@ -40,6 +40,12 @@ class Comment extends Base
         $dbman = DBManager::getInstance();
         return $dbman->query("SELECT *, CONCAT(".User::TABLE_NAME.".".User::NAME_KEY.", ' ', ".User::TABLE_NAME.".".User::SURNAME_KEY.") as user_fullname FROM ".self::TABLE_NAME." JOIN ".User::TABLE_NAME." on ".Comment::TABLE_NAME.".".Comment::USER_ID_KEY." = ".User::TABLE_NAME.".".Base::ID_KEY." WHERE ".self::TABLE_NAME.".".self::MEDIA_ID_KEY." = {$mediaId} ORDER BY ".self::TABLE_NAME.".".Base::CREATED_KEY." ASC;", Comment::class);
     }
+
+    public static function createComment($userId, $mediaId, $content)
+    {
+        $dbman = DBManager::getInstance();
+        return $dbman->query("INSERT INTO ".self::TABLE_NAME." (`".self::CONTENT_KEY."`, `".self::USER_ID_KEY."`, `".self::MEDIA_ID_KEY."`) VALUES ('{$content}', {$userId}, {$mediaId})");
+    }
  }
 
 ?>
