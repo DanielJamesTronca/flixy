87fff925df3022bd80ef6151be59ba84341dbf0c
Andrea Polo <andreapolo98@gmail.com>
2019-11-03 21:17:13 +0100

    inzio impostazione feed preferiti html+css feed

---
+++ website/public/feed.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
+
+<head>
+<base target="_self" href="http://localhost/flixy/website/public/">  <!--da cambiare!!!! -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"> <!-- per ottimizzazione visualizzazione smartphone -->
+<link rel="stylesheet" media="screen" type="text/css" href="assets/rules.css"/>
+<link rel="stylesheet" media="screen" type="text/css" href="assets/feed.css"/>
+<script type="text/javascript" src="action.js"></script>
+<title>Flixy - Feed</title>
+<meta name="title" content="Flixy - Login" />
+</head>
+
+<body>
+    <div class="content secondary-bg">
+        <h1 class="text-align-left padding-left-1-5 padding-top-1-5 font-weight-normal"> Your Feed </h1>  
+        <div id="container-next-release" class="margin-top-1-5 margin-left-2-5">
+            <div class="next-release">
+                <div>
+                    <img src="assets/images/covers/casa.jpg" class="cover"/>
+                </div>
+                <div class="padding-1 text-align-center"> 
+                    <h4>La casa de papel</h4>
+                    <h6>Stagione 3</h6>
+                </div>    
+            </div>
+            <div class="next-release">
+                <div>
+                    <img src="assets/images/covers/malef.jpg" class="cover"/>
+                </div>
+                <div class="padding-1 text-align-center">
+                    <h4>Maleficent</h4>
+                </div>   
+            </div>
+            <div class="next-release">
+               <div>
+                    <img src="assets/images/covers/casa.jpg" class="cover"/>
+                </div>
+                <div class="padding-1 text-align-center">
+                    <h4>La casa de papel</h4>
+                </div>   
+            </div>
+            
+            
+        </div>
+    </div>
+
+</body>
+
