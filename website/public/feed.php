092e4ad3f0e8984a0698b804fff0b01ab1ecfa9d
ANDREA-PC\Andrea <polo.andrea98@gmail.com>
2019-11-09 15:04:48 +0100

    aggiunto video in timeline, da rivedere allineamento

--- website/public/feed.php
+++ website/public/feed.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
 
 <head>
-<base target="_self" href="http://localhost/flixy/website/public/">  <!--da cambiare!!!! -->
+<base target="_self" href="http://localhost:8080/flixy/website/public/">  <!--da cambiare!!!! -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"> <!-- per ottimizzazione visualizzazione smartphone -->
 <link rel="stylesheet" media="screen" type="text/css" href="assets/rules.css"/>
@@ -63,18 +63,56 @@
                         <p class="padding-top-0-5">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
                     </div>
                 </div>
+                <div class="content-justify-right padding-top-1 padding-left-2">
+                        <img src="assets/images/covers/casa.jpg" class="timeline-image"></img>
+                </div>
             </div>
             <div class="timeline-container">
                 <div class="timeline-content">
-                <div class="timeline-date">
-      	            <p>12 Agosto</p>
+                    <div class="timeline-date">
+      	                <p>12 Agosto</p>
+                    </div>
+                    <div class="timeline-text">
+                        <h3> Glass </h3>
+                        <p> Rilasciato episodio 3x04 </p>
+                        <p class="padding-top-0-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar blandit purus vel bibendum. Nunc efficitur lorem euismod tincidunt eleifend. Quisque tristique, mi et vestibulum auctor, massa enim sagittis odio, non lacinia dolor nisi ut diam. Donec mauris magna, malesuada sed malesuada ut, aliquam vel ipsum. Fusce semper risus arcu, quis fermentum erat iaculis eget. Morbi viverra ipsum at porttitor faucibus. Cras blandit libero dapibus ultricies porttitor. mi et vestibulum auctor, massa enim sagittis odio, non lacinia dolor nisi ut diam. Donec mauris magna, malesuada sed malesuada ut,  mi et vestibulum auctor, massa enim sagittis odio, non lacinia dolor nisi ut diam. Donec mauris magna, malesuada sed malesuada ut,  mi et vestibulum auctor, massa enim sagittis odio, non lacinia dolor nisi ut diam. Donec mauris magna, malesuada sed malesuada ut.</p>
+                    </div>
                 </div>
-                <div class="timeline-text">
-                    <h3> Stranger Things </h3>
-                    <p> Rilasciato episodio 3x04 </p>
-                    <p class="padding-top-0-5">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                <div class="content-justify-right padding-top-1 padding-left-2">
+                        <img src="assets/images/covers/malef.jpg" class="timeline-image"></img>
                 </div>
             </div>  
+            <div class="timeline-container">
+                <div class="timeline-content">
+                    <div class="timeline-date">
+      	                <p>12 Agosto</p>
+                    </div>
+                    <div class="timeline-text">
+                        <h3> Stranger Things </h3>
+                        <p> Rilasciato episodio 3x04 </p>
+                        <p class="padding-top-0-5">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                    </div>
+                </div>
+                <div class="content-justify-right padding-top-1 padding-left-3">
+                    <object class="timeline-video" data="https://www.youtube.com/v/ZFlPFCd-K5k"> <!-- va aggiunto /v/ dopo .com e poi ID VIDEO -->
+                    </object>
+                </div>
+            </div>  
+            <div class="timeline-container">
+                <div class="timeline-content">
+                    <div class="timeline-date">
+      	                <p>12 Agosto</p>
+                    </div>
+                    <div class="timeline-text">
+                        <h3> Stranger Things </h3>
+                        <p> Rilasciato episodio 3x04 </p>
+                        <p class="padding-top-0-5">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                    </div>
+                </div>
+                <div class="content-justify-right padding-top-1 padding-left-2">
+                    <img src="assets/images/covers/casa.jpg" class="timeline-image"></img>
+                </div>
+            </div>   
         </div>  
 
     </div>
