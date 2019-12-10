6d623a02cf96c436e184e4c9921d123b2676552b
DanielJamesTronca <53578516+DanielJamesTronca@users.noreply.github.com>
2019-12-10 11:47:39 +0100

    Fixed path and variables

--- website/public/php/dettaglio.php
+++ website/public/php/dettaglio.php
@@ -1,8 +1,9 @@
 <?php
-include_once("../src/db_manager.php");
-include_once("../src/models/models.php");
-include_once("../src/session_manager.php");
+include_once("../../src/db_manager.php");
+include_once("../../src/models/models.php");
+include_once("../../src/session_manager.php");
 
+$output = file_get_contents("../html/dettaglio.html");
 
 $title="";
 $genre="None";
@@ -17,9 +18,8 @@ $trailer_url="None";
 
 $dbMan = DBManager::getInstance();
 $genreList = $dbMan->query("SELECT Genre.name FROM Genre LEFT JOIN Media ON Genre.id = Media.genre");
-console_log($genreList);
 
-function console_log ( $data) 
+function console_log ( $data)
 {
 echo '<script>';
 echo 'console.log(' .json_encode($data).')';
@@ -33,24 +33,22 @@ function loadInfo($id){
     $list=$dbMan->query("SELECT * FROM Media WHERE id='$id'") ;
     $title=$list[0]->name;
     $duration=$list[0]->duration;
+    
     $cover_url=$list[0]->cover_url;
     $description=$list[0]->description;
-
     $episode=$list[0]->episodes;
     $season=$list[0]->seasons;
     $trailer_url=$list[0]->trailer_url;
-   
-
-   
-
-    $arr=array($title,$duration, $cover_url, $description, $episode, $season, $trailer_url);
     
+    $arr=array($title,$duration, $cover_url, $description, $episode, $season, $trailer_url);
     return $arr;
+    
 }
+
 
 $lista= loadInfo(2);  // $movieId instead of 1
 
-$title=$lista[0];  
+$title=$lista[0];
 $duration=$lista[1];
 $cover_url=$lista[2];
 $description=$lista[3];
@@ -60,10 +58,34 @@ $trailer_url=$lista[6];
 
 
 
+$output=str_replace("{title}", $title,$output);
+$output=str_replace("{duration}", $duration,$output);
+$output=str_replace("{description}", $description,$output);
+$output=str_replace("{episode}", $episode,$output);
+$output=str_replace("{season}", $season,$output);
+$output=str_replace("{cover_url}", "../../public".$cover_url,$output);
+
+$output=str_replace("{trailer_url}",$trailer_url,$output);
+console_log($trailer_url);
+
+
+
+
+
+
+
+/*
+$duration=$lista[1];
+$cover_url=$lista[2];
+$description=$lista[3];
+$episode=$lista[4];
+$season=$lista[5];
+$trailer_url=$lista[6];
+*/
+
+echo $output;
+
 ?>
-
-
-
 
 
 
