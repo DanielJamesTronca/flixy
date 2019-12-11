dac031d70206ba09fd4b99eea7c7a575a85361a1
DanielJamesTronca <53578516+DanielJamesTronca@users.noreply.github.com>
2019-12-11 17:23:21 +0100

    started card on dettaglio.php

--- website/public/php/favourite_card.php
+++ website/public/php/favourite_card.php
@@ -1,55 +1,64 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
-<head>
-<base target="_self" href="http://localhost/flixy/website/public/"> <!--da cambiare!!!! -->
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"> <!-- per ottimizzazione visualizzazione smartphone -->
-<link rel="stylesheet" media="screen" type="text/css" href="prova.css"/>
-<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&amp;display=swap" rel="stylesheet" type="text/css"/>
-<script type="text/javascript" src="action.js"></script>
-<title>Flixy - Favourite Card</title>
-<meta name="title" content="Flixy - Favoutire card" />
-</head>
+<?php
+include_once("../../src/db_manager.php");
+include_once("../../src/models/models.php");
+include_once("../../src/session_manager.php");
+
+$card = file_get_contents("../html/favourite_card.html");
+
+
+$title="";
+$genre="None";
+$cover_url="None";
+
+
+$dbMan = DBManager::getInstance();
+$genreList = $dbMan->query("SELECT Genre.name FROM Genre LEFT JOIN Media ON Genre.id = Media.genre");
+
+function console_log ( $data)
+{
+echo '<script>';
+echo 'console.log(' .json_encode($data).')';
+echo '</script>';
+}
+
+
+function loadInfo($id){
+    $dbMan = DBManager::getInstance();
+
+    $list=$dbMan->query("SELECT * FROM Media WHERE id='$id'") ;
+
+    $title=$list[0]->name;
+    $cover_url=$list[0]->cover_url;
+    
+    $arr=array($title, $cover_url);
+    return $arr;
+   
+}
+
+$movieId=2;
+
+$lista= loadInfo($movieId);  // $movieId instead of 1
+
+$title=$lista[0];
+$cover_url=$lista[1];
+$genre=$genreList[$movieId-1]->name;
 
 
 
 
 
 
-  <body>
-    <div class="movie-entry">
 
-      <div class="movie">
-      
-      <?php 
-        echo '<img id="copertina" class="border-radius-medium copertina-size" src=".'.$url.'" alt="copertina"/>';
-      ?>
-
-      
+$card=str_replace("{movieTitle}", $title,$card);
+$card=str_replace("{movieGenre}", $genre,$card);
+$card=str_replace("{movieCover}", "../../public".$cover_url,$card);
+console_log($cover_url);
 
 
+echo $card;
 
-      </div>
+?>
 
-      <div id="genere" class="text-color-white margin-top-small">
-            <h1 id="text" class="margin-left-small font-medium-normal">Title</h1> 
-            <?php
-              echo $nome; 
-            ?>
-            <h2 id="text" class="margin-left-medium-small font-small">Genere</h2>
-            <?php
-              echo $genere; 
-            ?>
-      </div>
-      
 
-      <div id="fav-section" class="text-color-white">
-        <a><i id="fav-icon" class="fas fa-heart font-huge"></i></a>
-      </div>
-      
-      
-      
-    </div>
-  </body>
-</html>
+
 
