594f8f6f0cef974de231e7ff51a9d69fa97bfe89
mrampazz <mrampazz@gmail.com>
2020-01-13 19:52:27 +0100

    Messo le funzioni php di utils.pho dentro l'apposita classe in controllers\utils.php

--- website/public/php/layout.php
+++ website/public/php/layout.php
@@ -2,12 +2,10 @@
 include_once ("../../src/db_manager.php");
 include_once ("../../src/models/models.php");
 include_once ("../../src/session_manager.php");
-include_once ("./utils.php");
+include_once ("../../src/controllers/utils.php");
 
 $output = file_get_contents("../html/layout.html");
-$dbMan = DBManager::getInstance();
 
-$username = "";
 if (SessionManager::isUserLogged())
 {
     $username = SessionManager::getUsername();
@@ -19,9 +17,7 @@ if (SessionManager::isUserLogged())
     $output = str_replace("{logout_O_registrazione}", "Disconnettiti", $output);
     $output = str_replace("{profile_photo_url}", "../public".$user->avatarUrl, $output);
     $output = str_replace("{linkToFeed}", "./php/layout.php?page=feed", $output);
-}
-else
-{
+} else {
     $output = str_replace("{linkToFavs}", "./php/registrazione.php", $output);
     $output = str_replace("{link_to_profile_or_log_in}", "./php/login.php", $output);
     $output = str_replace("{login_O_username}", "Accedi", $output);
@@ -31,32 +27,18 @@ else
     $output = str_replace("{linkToFeed}", "./php/registrazione.php", $output);
 }
 
-// <form> logic
 if (isset($_GET["logout"])) {
     SessionManager::logout();
     header("Location: ".SessionManager::BASE_URL."home");
 }
 
-if (isset($_GET["search"]))
-{
+if (isset($_GET["search"])) {
     $varSearch = $_GET["search"];
-    $varReturnSearch = research($varSearch);
-}
-else
-{
+    $varReturnSearch = Utils::research($varSearch);
+} else {
     $varSearch = '';
     $varReturnSearch = '';
 }
-
-$registrazionePage = '"./php/registrazione.php"';
-$logOutAction = '$logOut';
-$signup = '<a id="sign-up" class="font-size-0-75 font-weight-light" href="./php/registrazione.php">';
-$logout = str_replace($registrazionePage, $logOutAction, $signup);
-
-
-
-$output = str_replace("{linkToFavs}", "./php/layout.php?page=profilo", $output);
-$output = str_replace("{linkToTrending}", "./php/layout.php?page=home", $output);
 
 switch ($_GET['page'])
 {
--- website/src/controllers/utils.php
+++ website/src/controllers/utils.php
@@ -211,6 +211,164 @@ class Utils {
         }
     }
 
+    // funzioni per layout.php e home.php
+    public static function research($input) {
+        if ($input) {
+          if (SessionManager::isUserLogged()) {
+            return Media::list(SessionManager::getUserId(), $input, null, null, 2, null, "ASC");
+          } else {
+            return Media::list(null, $input, null, null, 2, null, "ASC");
+          }
+        }
+    }
+
+    public static function generateMovieGenreOptions($genreList) {
+        $list = [];
+        for ($x = 0; $x < count($genreList); $x++) { 
+            $genre = $genreList[$x]->name;
+            if (isset($_GET["genre-select"])) {
+                if ($_GET["genre-select"] == $genre) {
+                $element = "<option selected value='".$genre."'>".$genre."</option>";
+                } else {
+                $element = "<option value='".$genre."'>".$genre."</option>";
+                }
+            } else {
+                $element = "<option value='".$genre."'>".$genre."</option>";
+            }
+            array_push($list, $element);
+        }
+        return implode($list);
+    }
+
+    public static function generateMovieYearOptions($yearList) {
+        $list = [];
+        for ($x = 0; $x < count($yearList); $x++) { 
+            $year = $yearList[$x]->anno;
+            if (isset($_GET["year-select"])) {
+                if ($_GET["year-select"] == $year) {
+                $element = "<option selected value=".$year.">".$year."</option>";
+                } else {
+                $element = "<option value='".$year."'>".$year."</option>";
+                }  
+            } else {
+                $element = "<option value='".$year."'>".$year."</option>";
+            }
+            array_push($list, $element);
+        }
+        return implode($list);
+    }
+
+    public static function generateMovieTypeOptions() {
+        $type1 = "Film";
+        $type2 = "Serie";
+        if (isset($_GET["type-select"])) {
+          if ($_GET["type-select"] == $type1) {
+            $element = "<option selected value='".$type1."'>".$type1."</option>"."<option value='".$type2."'>".$type2."</option>";
+          } 
+          if ($_GET["type-select"] == $type2) {
+            $element = "<option value='".$type1."'>".$type1."</option>"."<option selected value='".$type2."'>".$type2."</option>";
+          }  
+          if ($_GET["type-select"] == "All") {
+            $element = "<option value='".$type1."'>".$type1."</option>"."<option value='".$type2."'>".$type2."</option>";
+          }
+        } else {
+          $element = "<option value='".$type1."'>".$type1."</option>"."<option value='".$type2."'>".$type2."</option>";
+        }
+    
+        return $element;
+    }
+
+    public static function getFilteredMovieList($year, $genre, $type, $userId = null) {
+        if ($genre != "All") {
+            $genreId = Genre::getIdGenre($genre)[0]->id;
+        }
+        switch ($type) {
+            case "Film":
+                $typeId = 0;
+            break;
+            case "Serie":
+                $typeId = 1;
+            break;
+        }
+        return Media::list($userId, null, ($year != "All" ? $year : null), ($genre != "All" ? $genreId : null), ($type != "All" ? $typeId : 2), null, "ASC");
+    }
+
+    public static function checkLikedMovies($id) {
+        $votedMovies = null;
+        if (SessionManager::isUserLogged()) {
+            $userId = SessionManager::getUserId();
+            $votedMovies = Media::getUserVotes($userId);
+        }   
+        if ($votedMovies != null) {
+            for ($x = 0; $x < count($votedMovies); $x++) {
+                if ($votedMovies[$x]->media_id == $id) {
+                    switch ($votedMovies[$x]->positive) {
+                        case 1:
+                            return 1;
+                        break;
+                        case 0:
+                            return 0;
+                        break;
+                    }
+                }
+            }
+            return -1;
+        } else {
+            return -1;
+        }
+    }
+
+    public static function replaceContentsMovieCard($card, $title, $coverUrl, $stars, $id, $votesTotal, $votesPositive, $isFav) {
+        $starNumber = [];
+        if ($votesPositive == null) {
+          $card = str_replace("{likes}", 0, $card);
+        } else {
+          $card = str_replace("{likes}", $votesPositive, $card);
+        }
+        $movie = Media::fetch($id);
+        if ($movie->hasEpisodes == 1) {
+          $card = str_replace("{isMovie}", "Serie", $card);
+        } else {
+          $card = str_replace("{isMovie}", "Film", $card);
+        }
+        $card = str_replace("{movieTitle}", $title, $card);
+        $card = str_replace("{mediaNotFav}", !($isFav == true) ? "" : "hidden", $card);
+        $card = str_replace("{mediaIsFav}", ($isFav == true) ? "" : "hidden", $card);
+        $card = str_replace("{dislikes}", $votesTotal-$votesPositive, $card);
+        $card = str_replace("{coverURL}", "../public/".$coverUrl, $card);
+        $card = str_replace("{movieID}", $id, $card);
+        $card = str_replace("{linkDettaglioMovie}", "./php/layout.php?page=dettaglio&amp;movieId=".$id, $card);
+        $check = Utils::checkLikedMovies($id);
+        switch($check) {
+            case 1:
+                $card = str_replace("{like-selected}", "thumb-selected", $card);
+            break;
+            case 0:
+                $card = str_replace("{dislike-selected}", "thumb-selected", $card);
+            break;
+            case -1: {
+                $card = str_replace("{like-selected}", " ", $card);
+                $card = str_replace("{dislike-selected}", " ", $card);
+            } break;
+        }
+        for($i=0;$i<$stars;$i++) {
+            array_push($starNumber, "<i class='fa fa-star'></i>");
+        }
+        $card = str_replace("{movieStars}", implode($starNumber), $card);
+        return $card;
+    }
+
+    public static function generateMovieList($list) {
+        $movieList = [];
+        if (!is_array($list)) {
+          $list = [];
+        }
+        for ($x = 0; $x < count($list); $x++) {
+          $card = Utils::replaceContentsMovieCard(file_get_contents("../html/movie-card.html"), $list[$x]->title, $list[$x]->coverUrl, $list[$x]->stars, $list[$x]->id, $list[$x]->votesTotal, $list[$x]->votesPositive, $list[$x]->isFavourite);
+          array_push($movieList, $card);
+        }
+        return implode($movieList);
+    }
 }
 
 
