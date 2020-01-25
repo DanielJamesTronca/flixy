c046fe935045c48ac8d42fd788ffce427510cd82
Andrea Polo <andreapolo98@gmail.com>
2020-01-25 10:18:20 +0100

    fix range data select anno

--- website/src/controllers/utils.php
+++ website/src/controllers/utils.php
@@ -66,38 +66,10 @@ class Utils {
     }
 
     public static function generateOptionsDay(){
-        return
-        "<option value='1'>1</option>
-        <option value='2'>2</option>
-        <option value='3'>3</option>
-        <option value='4'>4</option>
-        <option value='5'>5</option>
-        <option value='6'>6</option>
-        <option value='7'>7</option>
-        <option value='8'>8</option>
-        <option value='9'>9</option>
-        <option value='10'>10</option>
-        <option value='11'>11</option>
-        <option value='12'>12</option>
-        <option value='13'>13</option>
-        <option value='14'>14</option>
-        <option value='15'>15</option>
-        <option value='16'>16</option>
-        <option value='17'>17</option>
-        <option value='18'>18</option>
-        <option value='19'>19</option>
-        <option value='20'>20</option>
-        <option value='21'>21</option>
-        <option value='22'>22</option>
-        <option value='23'>23</option>
-        <option value='24'>24</option>
-        <option value='25'>25</option>
-        <option value='26'>26</option>
-        <option value='27'>27</option>
-        <option value='28'>28</option>
-        <option value='29'>29</option>
-        <option value='30'>30</option>
-        <option value='31'>31</option>";
+        $toReturn = "";
+        for ($i=1; $i<=31; $i++)
+            $toReturn.= "<option value='$i'>$i</option>";
+        return $toReturn;
     }
 
     public static function generateOptionsMonth(){
@@ -117,20 +89,16 @@ class Utils {
     }
 
     public static function generateOptionsYear(){
-        return
-        "<option value='2019'>2019</option>
-        <option value='2020'>2020</option>
-        <option value='2021'>2021</option>
-        <option value='2022'>2022</option>
-        <option value='2023'>2023</option>
-        <option value='2024'>2024</option>
-        <option value='2025'>2025</option>";
+        $toReturn = "";
+        for ($i=2000; $i<=2025; $i++)
+            $toReturn.= "<option value='$i'>$i</option>";
+        return $toReturn;
     }
 
     public static function restoreOptionsYear($yearToRestore){
         $options = "";
         $toRestore = "";
-        for ($i=2019; $i<=2025; $i++){
+        for ($i=2000; $i<=2025; $i++){
             if ($i == $yearToRestore)
                     $toRestore = "<option value='$i'>$i</option><optgroup class='secondary-bg' label='--------'>";
             else
